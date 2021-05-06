8ec6770797065504f0c193efd79b4e3ce2b0bb76
Khloe-joanie <khloejoanne@gmail.com>
2021-05-06 15:13:01 +0300

    Changes made to SidebarData

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,22 +1,26 @@
 /** @format */
 
-export const SidebarData = [
+const SidebarData = [
   {
+    id: "1",
     title: "Home",
     path: "/",
     cName: "nav-text",
   },
   {
+    id: "2",
     title: "About",
     path: "/about",
     cName: "nav-text",
   },
   {
+    id: "3",
     title: "Medical Staff",
     path: "/ourstaff",
     cName: "nav-text",
   },
   {
+    id: "4",
     title: "Contact",
     path: "/contact",
     cName: "nav-text",
